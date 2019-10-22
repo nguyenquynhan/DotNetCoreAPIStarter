423dc52442e977d48e8608d6d584a52dbcb79f25
Nhan Nguyen <Nhan.Nguyen@soldenservices.com>
2019-10-22 14:00:30 +0700

    Clean TodoItem entity

--- NetCoreTodoApi/Entities/TodoContext.cs
+++ NetCoreTodoApi/Entities/TodoContext.cs
@@ -15,34 +15,15 @@ namespace NetCoreTodoApi.Entities
         {
         }
 
-        public virtual DbSet<TodoItem> TodoItems { get; set; }
         public virtual DbSet<User> Users { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-
-        }
-
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
 
-            modelBuilder.Entity<TodoItem>(entity =>
-            {
-                entity.ToTable("TodoItem");
-
-                entity.Property(e => e.Id).ValueGeneratedNever();
-
-                entity.Property(e => e.Name)
-                    .IsRequired()
-                    .HasMaxLength(50);
-            });
-
             modelBuilder.Entity<User>(entity =>
             {
                 entity.ToTable("User");
-
-                entity.Property(e => e.Id);
 
                 entity.Property(e => e.FirstName).HasMaxLength(50);
 
